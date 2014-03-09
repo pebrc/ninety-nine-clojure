894e7b79a97242b742e46ae227fa6e1a5d03f773
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-03-09 17:44:44 +0100

    Added solution to problem 32: Determine the greatest common divisor of two positive integer numbers

--- src/ninety_nine_clojure/arithmetic.clj
+++ src/ninety_nine_clojure/arithmetic.clj
@@ -4,3 +4,10 @@
 (defn prime? [x]
   "P31 (**) Determine whether a given integer number is prime."
   (empty?  (filter #(= 0 (rem x %)) (range 2 (inc (int (math/sqrt x)))))))
+
+(defn gcd [x y]
+  "P32 (**) Determine the greatest common divisor of two positive
+  integer numbers."
+  (if (= y 0)
+    x
+    (gcd y (rem x y))))
--- test/ninety_nine_clojure/arithmetic_test.clj
+++ test/ninety_nine_clojure/arithmetic_test.clj
@@ -5,3 +5,9 @@
 (deftest p31-test-primes
   (is (not (prime? 4)))
   (is (prime? 7)))
+
+(deftest p32-euclids-algorithm
+  (is (= 9 (gcd 36 63)))
+  (is (= 21 (gcd 462 1071)))
+  (is (= 1  (gcd 1 1))))
+
