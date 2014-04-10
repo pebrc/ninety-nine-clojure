6046f79393fa4139f79cf37921947daed03dd06d
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-04-10 10:29:43 +0200

    Added fermat test based on SICP 1.2.6 for problem 31

--- src/ninety_nine_clojure/arithmetic.clj
+++ src/ninety_nine_clojure/arithmetic.clj
@@ -1,9 +1,29 @@
 (ns ninety-nine-clojure.arithmetic
   (:require [clojure.math.numeric-tower :as math]))
 
+
 (defn prime? [x]
   "P31 (**) Determine whether a given integer number is prime."
   (empty?  (filter #(= 0 (rem x %)) (range 2 (inc (int (math/sqrt x)))))))
+
+(defn square [x] (* x x))
+
+(defn expmod [base exp m]
+  (cond (= exp 0) 1
+        (even? exp) (rem (square (expmod base (/ exp 2) m )) m)
+        :else (rem (* base (expmod base (- exp 1) m )) m )))
+
+(defn fermat-test [n]
+  (letfn [(try-it [r]
+            (= (expmod r n n) r))]
+    (try-it (inc (rand-int (dec n))))))
+
+(defn fast-prime? [x]
+  "P31 (**) Determine whether a given integer number is prime. This
+  uses the fermat test from SICP (which fails for Carmichael numbers).
+  Also: the number ofo 100 tests is arbitrary)"
+  (empty?  (filter not (repeatedly 100  #(fermat-test x)))))
+
 
 (defn gcd [x y]
   "P32 (**) Determine the greatest common divisor of two positive
--- test/ninety_nine_clojure/arithmetic_test.clj
+++ test/ninety_nine_clojure/arithmetic_test.clj
@@ -4,7 +4,14 @@
 
 (deftest p31-test-primes
   (is (not (prime? 4)))
-  (is (prime? 7)))
+  (is (prime? 7))
+  (is (fast-prime? 7))
+  (is (not (fast-prime? 4))))
+
+(deftest p31-fermat-fails-on-carmichael
+  "561 is a Carmichael number and a incorrectly identified as prime by
+  tests based on Fermat's Little Theorem"
+  (is (fast-prime? 561)))
 
 (deftest p32-euclids-algorithm
   (is (= 9 (gcd 36 63)))
