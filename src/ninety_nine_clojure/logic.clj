f631de011d6529ea9a98657df4d7d82b755cc520
Peter Brachwitz <peter.brachwitz@gmail.com>
2014-08-17 11:14:37 +0100

    Solution to P48 (**) Truth tables for logical expressions (3).

--- src/ninety_nine_clojure/logic.clj
+++ src/ninety_nine_clojure/logic.clj
@@ -1,5 +1,6 @@
 (ns ninety-nine-clojure.logic
-  (:require [clojure.pprint :only print-table]))
+  (:require [clojure.pprint :only print-table]
+            [clojure.math.combinatorics :as combo]))
 
 (defn and-fn [x y]
   (if x (if y y false) false))
@@ -24,20 +25,26 @@
   (or (not x)
       (and x y)))
 
-(def inputs [[true true] [true false] [false true] [false false]])
+(def inputs [true false])
+
+(defn gen-inputs [arity]
+  (apply combo/cartesian-product (repeat arity inputs)))
 
 (defmacro evaluator [expr]
   `(fn [& args#]
      (eval `(~'~expr ~@args#))))
 
 
-(defmacro table [expr]
-  `(let [header#  [:a :b :result]
-         ev# (evaluator ~expr)
-         rows# (->> inputs
-                    (map (fn [x#] (interleave header# (flatten [x# (apply ev# x#)] ))))
-                    (map (fn [x#] (apply sorted-map x#))))]
-     (clojure.pprint/print-table rows#)))
+(defmacro table
+  ([expr]
+     `(table (first (:arglists (meta ~expr))) ~expr))
+  ([arglist expr]     
+     `(let [header#  (concat (map keyword ~arglist) [:result])
+            ev# (evaluator ~expr)
+            rows# (->> (gen-inputs (count ~arglist))
+                       (map (fn [x#] (interleave header# (flatten [x# (apply ev# x#)] ))))
+                       (map (fn [x#] (apply sorted-map x#))))] 
+        (clojure.pprint/print-table rows#))))
 
 (def unary? {'not true})
 
@@ -58,17 +65,15 @@
   ([a b c d & more]
      (cond
       (and (unary? d) (unary? a)) `(~c (~a (infix ~b)) (~d (infix ~more)))
-      (or (unary? c) (unary? a)) (let [exp (first more)
+      (xor (unary? c) (unary? a)) (let [exp (first more)
                                        tail (rest more)]
                                     `(~exp (infix ~a ~b ~c ~d) (infix ~tail)))
-       :else  `(~d (~b (infix ~a) (infix ~c)) (infix ~@more)))))
+      :else  `(~d (~b (infix ~a) (infix ~c)) (infix ~@more)))))
+
 
 (defmacro i
   "Creates a Clojure function from a logical expression in infix
   syntax like 'a and b'. Binds two variable a and b by convention"
-  [& args]
-  
-  (let [a (gensym)
-        b (gensym)
-        code (clojure.walk/postwalk-replace {'a a 'b b} args)]
-    `(fn [~a ~b] (infix ~@code))))
+  [bindings & args]  
+  (let [bindingslist (list bindings) ]
+    `(with-meta (fn  ~bindings (infix ~@args)) {:arglists '~bindingslist})))
