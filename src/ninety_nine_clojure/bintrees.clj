98d2cef9b26224a074e2ff10b41e2dc0312da712
Peter Brachwitz <peter.brachwitz@gmail.com>
2015-05-03 11:44:07 +0200

    Added solution for P55

--- src/ninety_nine_clojure/bintrees.clj
+++ src/ninety_nine_clojure/bintrees.clj
@@ -7,3 +7,34 @@
          nil                                       true
          :else                                     false))
 
+
+
+(defn balanced-trees
+  "P55 (**) Construct completely balanced binary trees In a completely
+  balanced binary tree, the following property holds for every node:
+  The number of nodes in its left subtree and the number of nodes in
+  its right subtree are almost equal, which means their difference is
+  not greater than one.
+
+  Write a predicate cbal_tree/2 to construct completely balanced binary
+  trees for a given number of nodes. The predicate should generate all
+  solutions via backtracking. Put the letter 'x' as information into
+  all nodes of the tree. 
+
+  Example: ?- cbal_tree(4,T). T = t(x, t(x,
+  nil, nil), t(x, nil, t(x, nil, nil))) ; T = t(x, t(x, nil, nil),
+  t(x, t(x, nil, nil), nil)) ; etc......No"
+  [n v]
+  (let [rem (rem (int n) 2)
+        map-trees (fn ([subtrees]
+                     (mapcat (fn [l] (map (fn [r] (vector v l r)) subtrees))subtrees) )
+                    ([lefts rights]
+                     (mapcat (fn [l] (mapcat (fn [r] [(vector v l r) (vector v r l)]) rights))lefts)))]
+    (cond
+      (< n 1)  [nil] 
+      (= rem 1) (let [subtrees (balanced-trees (/ n 2) v)]
+           (map-trees subtrees))
+      (= rem 0)  (let [lower (balanced-trees (/ (dec n) 2) v)
+                       higher (balanced-trees (inc (/ (dec n)  2)) v)]
+                   (map-trees  lower higher )) )) )
+    
