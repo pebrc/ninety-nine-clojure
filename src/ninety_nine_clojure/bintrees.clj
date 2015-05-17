69053b1b606bd0b5aa28e54dfbf71830621eee92
Peter Brachwitz <peter.brachwitz@gmail.com>
2015-05-17 12:47:42 +0200

    P56--first version: ambiguous nil semantics

--- src/ninety_nine_clojure/bintrees.clj
+++ src/ninety_nine_clojure/bintrees.clj
@@ -55,14 +55,32 @@
         n2  (- n0 n1)
         distrib (fn
                   ([subtrees]
-                   (mapcat (fn [l] (map (fn [r] (vector v l r)) subtrees))subtrees) )
+                   (mapcat (fn [l] (map (fn [r] (vector v l r)) subtrees)) subtrees) )
                   ([lefts rights]
-                   (mapcat (fn [l] (mapcat (fn [r] [(vector v l r) (vector v r l)]) rights))lefts)))]
+                   (mapcat (fn [l] (mapcat (fn [r] [(vector v l r) (vector v r l)]) rights)) lefts)))]
     (cond
       (< n 1)  [nil] 
-      (= n1 n2) (let [subtrees (balanced-trees n1 v)]
-                  (distrib subtrees))
+      (= n1 n2) (distrib (balanced-trees n1 v))
       :odd  (let [lower (balanced-trees n1  v)
                   higher (balanced-trees n2 v)]
               (distrib lower higher )) )) )
+
+(defn mirror? [l r]
+  {:pre [(tree? l) (tree? r)]}
+  (cond
+    (= nil l r) true
+    (and (mirror? (lefts l) (rights r)) (mirror? (rights l) (lefts r))) true
+    :else false))
+
+(defn symmetric?
+  "P56 (**) Symmetric binary trees Let us call a binary tree symmetric
+  if you can draw a vertical line through the root node and then the
+  right subtree is the mirror image of the left subtree. Write a
+  predicate symmetric/1 to check whether a given binary tree is
+  symmetric. Hint: Write a predicate mirror/2 first to check whether
+  one tree is the mirror image of another. We are only interested in
+  the structure, not in the contents of the nodes."
+  [t]
+  {:pre [(tree? t)]}
+  (mirror? (lefts t) (rights t)))
     
