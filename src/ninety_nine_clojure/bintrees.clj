0cca97b04944d390553e101da322f18e0a5d3add
Peter Brachwitz <peter.brachwitz@gmail.com>
2016-02-20 13:05:19 +0100

    P62B reformatted

--- src/ninety_nine_clojure/bintrees.clj
+++ src/ninety_nine_clojure/bintrees.clj
@@ -207,12 +207,12 @@
   (map first (filter branch? (depth-first t))))
 
 
-(defn at-level [t l]
+(defn at-level 
   "P62B (*) Collect the nodes at a given level in a list.
-A node of a binary tree is at level N if the path from the root to the
-node has length N-1. The root node is at level 1. Write a method
-at-level to collect all nodes at a given level in a list.
-"
+  A node of a binary tree is at level N if the path from the root to the
+  node has length N-1. The root node is at level 1. Write a method
+  at-level to collect all nodes at a given level in a list."
+  [t l]
   (let [offset (dec (m/expt 2 (dec l)))]
     (map first (take (inc offset)  (drop offset (depth-first t))))))
 
